62a1bbc888460801be042cec4b23871546f2f512
SimpleTechDev1 <programmingkalsel.error@gmail.com>
2022-05-29 17:57:24 +0800

    foreign keys

--- database/migrations/2022_05_29_071506_create_permission_role_table.php
+++ database/migrations/2022_05_29_071506_create_permission_role_table.php
@@ -15,8 +15,8 @@ class CreatePermissionRoleTable extends Migration
     {
         Schema::create('permission_role', function (Blueprint $table) {
             $table->id();
-            $table->integer('permission_id');
-            $table->integer('role_id');
+            $table->foreignId('permission_id')->nullable()->index('fk_role_user_to_permission');
+            $table->foreignId('role_id')->nullable()->index('fk_role_user_to_role');
             $table->timestamps();
             $table->softDeletes();
         });
